03c9dbfad1ab90523d1a31e605c87d818bf10866
Patrick Donelan <pat@patspam.com>
2010-04-08 13:41:44 -0400

    Now does Australian translation

--- lib/wG_au.pm
+++ lib/wG_au.pm
@@ -3,17 +3,76 @@ package wG_au;
 
 use warnings;
 use strict;
+use Path::Class;
+use File::Spec;
+use File::Slurp;
+use File::ShareDir qw(dist_dir);
+use File::Copy;
+use File::Find;
+use Perl::Tidy;
+use autodie;
+use Data::Dumper;
 
-=head1 SYNOPSIS
+=head1 DESCRIPTION
 
-    use wG_au;
+Creates an Australian translation from the default WebGUI "English" translation
+
+    wG_au [target_dir]
 
 =cut
 
 sub translate {
+    my ($class, $root, $to) = @_;
     
+    # Create sub-folder
+    
+    my $subdir = File::Spec->catfile($to, 'Australian');
+    my $i18n = File::Spec->catfile($root, 'lib/WebGUI/i18n/');
+    my $en = File::Spec->catfile($i18n, 'English');
+    my $now = time;
+    
+    mkdir $subdir unless -e $subdir;
+    
+    find(sub {
+        my $file = $_;
+        return unless $file =~ m{\.pm$};
+        
+        my $package = $file;
+        $package =~ s{\.pm$}{};
+        print "Translating $package..\n";
+        my $i = eval "\$WebGUI::i18n::English::${package}::I18N";
+        for my $v (values %$i) {
+            $v->{lastUpdated} = $now;
+            $v->{message} = $class->translate_phrase($v->{message});
+        }
+        $Data::Dumper::Terse = 1;
+        $Data::Dumper::Sortkeys = 1;
+        my $dump = Dumper($i);
+        my $t = File::Spec->catdir($subdir, $file);
+        
+        my $contents = <<"END_CONTENTS";
+package WebGUI::i18n::Australian::$package;
+# ABSTRACT: Australian WebGUI (auto) Translation
+use strict;
+our \$I18N = $dump;
+1;
+END_CONTENTS
+        Perl::Tidy::perltidy( source => \$contents, destination => $t);
+    }, $en);
+    
+    # Create the Language definition file
+    my $defn = File::Spec->catfile($to, 'Australian.pm');
+    print "Creating language definition $defn\n";
+    my $share = dist_dir(__PACKAGE__);
+    copy(File::Spec->catfile($share, 'Australian.tmpl'), File::Spec->catfile($to, 'Australian.pm'))
+        or die $!;
+    
+    print "Finished!\n";
 }
 
-sub cheers(){1};
+sub translate_phrase {
+    my ($class, $phrase) = @_;
+    return "G'day! $phrase";
+}
 
-cheers;
+1;
---
+++ t/run.t
@@ -0,0 +1,12 @@
+use strict;
+use warnings;
+
+use wG_au;
+use Test::More tests => 2;
+use File::Temp;
+use Test::File;
+
+my $dir = File::Temp->newdir;
+wG_au->translate('/data/WebGUI', $dir);
+file_exists_ok(File::Spec->catfile($dir, 'Australian.pm'));
+file_exists_ok(File::Spec->catfile($dir, 'Australian', 'Workflow.pm'));
